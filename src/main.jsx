61337dfd4b5903d54f21cacfb737a1dd22d1e112
Adam Abdallah <adamabdallahayln1@gmail.com>
2026-09-08 18:29:18 +0300

    feat: add Vercel Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import ErrorBoundary from "./components/ErrorBoundary";
 import App from './App.jsx'
@@ -7,5 +8,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
     <ErrorBoundary>
         <App />
+        <Analytics />
     </ErrorBoundary>
 )
